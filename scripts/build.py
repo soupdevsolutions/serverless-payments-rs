af0ec743ff802519d243b703b274594a7626e9ef
mirch <mirceateodor.oprea@gmail.com>
2023-08-13 16:51:56 +0300

    Add pipelines

---
+++ scripts/build.py
@@ -0,0 +1,16 @@
+# builds the lambda functions and copies them to the infrastructure folder
+
+import os
+import sys
+
+commands = [
+    "cargo install cargo-lambda",
+    "cargo lambda build --release"
+]
+os.system("; ".join(commands))
+
+functions = [name for name in os.listdir("target/lambda/")]
+
+commands = [f"cp target/lambda/{function}/bootstrap infrastructure/data/lambdas/{function}" for function in functions"]
+os.system("; ".join(commands))
+
