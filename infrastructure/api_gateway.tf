43abb6a6cd699091278b760fecfc935d1a702a71
mirch <mirceateodor.oprea@gmail.com>
2023-08-18 17:11:09 +0300

    Add deployment id to stage

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -15,6 +15,7 @@ resource "aws_apigatewayv2_stage" "api_stage" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+  deployment_id = aws_apigatewayv2_deployment.api_deployment.id
 }
 
 resource "aws_apigatewayv2_deployment" "api_deployment" {
