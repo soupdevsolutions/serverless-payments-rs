49465aec3d6b3bd9a050e78080bccc04e9cb1ce1
mirch <mirceateodor.oprea@gmail.com>
2023-08-18 18:07:29 +0300

    Fix lambda handlers

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "initiate_payment_lambda" {
   source_code_hash = filebase64sha256("data/lambdas/initiate_payment.zip")
   filename         = "data/lambdas/initiate_payment.zip"
 
-  handler = "func"
+  handler = "handler"
   runtime = "provided"
 
   role = aws_iam_role.initiate_payment.arn
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "finish_payment_lambda" {
   source_code_hash = filebase64sha256("data/lambdas/finish_payment.zip")
   filename         = "data/lambdas/finish_payment.zip"
 
-  handler = "func"
+  handler = "handler"
   runtime = "provided"
 
   role = aws_iam_role.finish_payment.arn
