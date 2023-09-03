89e65deffe006958adb461cf066c3ce8c6f90ae5
mirch <mirceateodor.oprea@gmail.com>
2023-09-03 17:42:53 +0300

    Add more comments to initiate_payment

--- src/initiate_payment.rs
+++ src/initiate_payment.rs
@@ -25,18 +25,21 @@ async fn handler(event: Request) -> Result<Response<Body>, Error> {
     // Get the payment request from the event
     let payment_request: InitiatePaymentRequest = get_body(&event)?;
 
+    // Create the Payment object with a unique id
     let payment = Payment::new(
         Uuid::new_v4().to_string(),
         payment_request.amount,
-        payment_request.sender.clone(),
+        payment_request.sender,
         PaymentStatus::Pending,
     );
 
     // Send the event to Stripe
     let payment_client = PaymentClient::new();
+    // Get the redirect URL from the `initiiate payment` process
     let redirect_url = payment_client.initiate_payment(&payment).await?;
-    // Save the data in DynamoDB
+    // Get the singleton instance of the payments repository
     let payments_repository = PaymentsRepository::get().await;
+    // Save the data in DynamoDB
     payments_repository.insert_payment(payment).await?;
 
     // Return the redirect URI
